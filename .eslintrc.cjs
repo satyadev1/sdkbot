b6f27f9b8f3a8d8248f5c1f15847b81ea93cd129
Satyadev K <Komaragirisatyadev1007@gmail.com>
2026-08-27 18:55:06 +0530

    chore: scaffold TypeScript project, Postgres via Docker Compose

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: { project: './tsconfig.json', sourceType: 'module' },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  env: { node: true, es2022: true },
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+  },
+};
