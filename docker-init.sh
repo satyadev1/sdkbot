1039e11a574adbe628f7adbb177b1861ef8136a4
Satyadev K <Komaragirisatyadev1007@gmail.com>
2026-08-27 19:35:32 +0530

    fix: revert unrequested docker-compose/auth changes from Task 8

--- docker-init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Fix pg_hba.conf for development - allow TCP connections with trust auth
-
-sed -i 's/host all all all scram-sha-256/host all all all trust/g' "$PGDATA/pg_hba.conf"
-
-echo "pg_hba.conf updated to allow TCP connections"
